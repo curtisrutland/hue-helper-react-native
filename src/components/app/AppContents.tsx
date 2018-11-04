0ea5b81f40312706d8e18454426da2871aabdaf4
Curtis Rutland <curtisrutland@gmail.com>
2018-11-04 01:43:06 -0600

    style ejected

--- src/components/app/AppContents.tsx
+++ src/components/app/AppContents.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import { Root, Container, Content } from "native-base";
+import { Root, Container, Content, StyleProvider } from "native-base";
 import AppHeader from "./AppHeader";
 import Discovery from "../hue/Discovery";
 
+import getTheme from "../../theme/components";
+import commonColor from "../../theme/variables/commonColor";
+
 const AppContents: React.SFC = () => {
+    const theme = getTheme(commonColor);
     return (
-        <Root>
-            <Container>
-                <AppHeader />
-                <Content padder>
-                    <Discovery />
-                </Content>
-            </Container>
-        </Root>
+        <StyleProvider style={theme}>
+            <Root>
+                <Container>
+                    <AppHeader />
+                    <Content padder>
+                        <Discovery />
+                    </Content>
+                </Container>
+            </Root>
+        </StyleProvider>
     )
 }
 
